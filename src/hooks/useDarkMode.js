fbfee005b8e087443738d5591cf30f04179a7cca
Reagan Barrington <reagan@barringtonfamily.net>
2020-03-11 16:13:14 -0400

    useDarkMode

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -4,8 +4,5 @@ import { render } from "@testing-library/react";
 const useDarkMode = (key) => {
     const [someValue, setSomeValue] = useLocalStorage(key);
 
-    render()
-    return (
-        <h1>H1 here</h1>
-    )
+  
 }
